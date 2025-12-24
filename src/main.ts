e0a4ac7c1ab8b566f15128581f790d15bd069613
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2025-12-24 12:59:10 +0500

    resolved CORS issue

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: "*", // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
